ba6d1223cead1663c17a89c8da835b2ec787158e
andrea.longobardi85 <andrea.longobardi85@gmail.com>
2018-07-02 16:47:59 +0200

    mods on plugin

--- KiCad/KiCad_Plugins/action_menu_generate_assembly_drawings.py
+++ KiCad/KiCad_Plugins/action_menu_generate_assembly_drawings.py
@@ -4,6 +4,8 @@
 #
 
 import pcbnew
+import csv
+import os
 
 class generate_assembly_drawings( pcbnew.ActionPlugin ):
     """
@@ -23,31 +25,45 @@ class generate_assembly_drawings( pcbnew.ActionPlugin ):
         self.description = "Generates TOP and BOTTOM assembly drawings"
 
     def Run( self ):
-        pcb = pcbnew.GetBoard()
+		pcb = pcbnew.GetBoard()
+		pcb_path = os.path.dirname(pcb.GetFileName())
+			
+		pctl = pcbnew.PLOT_CONTROLLER(pcb)
+			
+		popt = pctl.GetPlotOptions()
+			
+		popt.SetOutputDirectory("Outputs/Assembly/")
+			
+		# Set some important plot options:
+		popt.SetPlotFrameRef(True)
+		popt.SetLineWidth(pcbnew.FromMM(0.1))
+			
+		popt.SetAutoScale(False)
+		popt.SetScale(1)
+		popt.SetMirror(False)
+		popt.SetExcludeEdgeLayer(False)
+		popt.SetPlotValue(False)
+		popt.SetPlotReference(True)
+			
+		#Create a pdf file of the top fab Layer
+		pctl.SetLayer(pcbnew.F_Fab)
+		pctl.OpenPlotfile("Assembly_TOP", pcbnew.PLOT_FORMAT_PDF, "Assembly_TOP")
+		pctl.PlotLayer()
 		
-	pctl = pcbnew.PLOT_CONTROLLER(pcb)
+		#Create a pdf file of the bottom fab Layer
+		pctl.SetLayer(pcbnew.B_Fab)
+		pctl.OpenPlotfile("Assembly_BOTTOM", pcbnew.PLOT_FORMAT_PDF, "Assembly_BOTTOM")
+		pctl.PlotLayer()
 		
-	popt = pctl.GetPlotOptions()
+		pctl.ClosePlot()
 		
-	popt.SetOutputDirectory("Outputs/Assembly/")
+		file = open('%s/position.csv' % pcb_path, 'w')
+		writer = csv.writer(file, dialect='excel')
 		
-	# Set some important plot options:
-	popt.SetPlotFrameRef(True)
-	popt.SetLineWidth(pcbnew.FromMM(0.1))
-		
-	popt.SetAutoScale(False)
-	popt.SetScale(1)
-	popt.SetMirror(False)
-	popt.SetExcludeEdgeLayer(False)
-	popt.SetPlotValue(False)
-	popt.SetPlotReference(True)
-        
-	#Create a pdf file of the top silk layer
-	pctl.SetLayer(pcbnew.F_Fab)
-	pctl.OpenPlotfile("Assembly_TOP", pcbnew.PLOT_FORMAT_PDF, "Assembly_TOP")
-	pctl.PlotLayer()
-	
-	pctl.ClosePlot()
+		for m in pcbnew.GetBoard().GetModules():
+			c = m.GetCenter()
+			writer.writerow((m.GetReference(), c.x, c.y))
+		file.close()
 
 
 generate_assembly_drawings().register()
